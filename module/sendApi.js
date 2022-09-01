bde4e3be89c734f5ee82ed977d08deb890f40bba
Benjamin <benmma13@gmail.com>
2022-09-01 23:17:52 +0400

    fix linters

--- module/sendApi.js
+++ module/sendApi.js
@@ -7,7 +7,7 @@ async function sendAPi() {
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-    }
-  }).then((param)=> param.json()).then((saved) => saved);
+    },
+  }).then((param) => param.json()).then((saved) => saved);
 }
 sendAPi();
--- module/sendScores.js
+++ module/sendScores.js
@@ -4,11 +4,11 @@ async function sendAPi(arg1, arg2) {
     method: 'POST',
     body: JSON.stringify({
       user: arg1,
-      score: arg2
+      score: arg2,
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  }).then((param)=> param.json()).then((saved) => saved);
+  }).then((param) => param.json()).then((saved) => saved);
 }
 export default sendAPi;
